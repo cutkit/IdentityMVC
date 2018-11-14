149fcffb55cb4f129766f0912d943180f3ee76db
cutkit49 <tungld702@gmail.com>
2018-11-15 03:26:25 +0700

    Add project files.

---
+++ AuthenticationMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthenticationMVC.Startup))]
+namespace AuthenticationMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
