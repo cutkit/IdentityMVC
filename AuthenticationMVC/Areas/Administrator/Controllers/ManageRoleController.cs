da63dedfd06b0354e4396b42e098c61f37b7aceb
cutkit49 <tungld702@gmail.com>
2018-11-16 03:35:07 +0700

    roles

--- AuthenticationMVC/Areas/Administrator/Controllers/ManageRoleController.cs
+++ AuthenticationMVC/Areas/Administrator/Controllers/ManageRoleController.cs
@@ -1,7 +1,9 @@
 ﻿using AuthenticationMVC.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +16,35 @@ namespace AuthenticationMVC.Areas.Administrator.Controllers
         public ActionResult Index()
         {
             var model = db.Roles.AsEnumerable();
+            //foreach (var item in model)
+            //{
+            //    item.
+            //}
             return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(IdentityRole role)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Roles.Add(role);
+                    await db.SaveChangesAsync();
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(role);
+            }
+
         }
     }
 }
